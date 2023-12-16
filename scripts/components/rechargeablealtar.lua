21673911c8cd9f7f04dbbade4d8b5b13d80c6bb2
Gamnraa <ltaz1466@gmail.com>
2023-12-16 14:03:15 -0500

    Should be all the basic logic we need for the component to receive fuel and charge; and expend charges
    We should aim to keep the structure simple so that it may be replicated across multiple prefabs that need the same basic structure. In other words, the Altar prefab shall handle the logic for what happens when it is charged.

--- scripts/components/rechargeablealtar.lua
+++ scripts/components/rechargeablealtar.lua
@@ -5,11 +5,37 @@ local RechargeableAltar = Class(function(self, inst)
     self.maxfuel = 100
     self.currentfuel = 100
     self.accepteditems = {} --format the table to be k = prefabname, v = amount to fuel
+    self.accepting = true
+    self.onreceivechargefn = nil
 end)
 
 function RechargeableAltar:IsCharged() return self.currentcharges > 0 end
+
+function RechargeableAltar:SetMaxCharges(amount)
+    self.maxcharges = amount
+end
+
+function RechargeableAltar:SetMaxFuel(amount)
+    self.maxfuel = amount
+end
+
+function RechargeableAltar:SetAcceptedItems(items)
+    self.acceptitems = items
+end
 
 function RechargeableAltar:UseCharge()
     self.currentcharges = self.currentcharges - 1
     self.currentfuel = 0
 end
+
+function RechargeableAltar:ReceiveFuel(item)
+    self.currentfuel = self.currentfuel + self.accepteditems[item]
+    if self.currentfuel >= self.maxfuel then
+        self.currentcharges = self.currentcharges + 1
+        self.currentfuel = 0
+        self.accepting = self.currentcharges < self.maxcharges
+        if self.onreceivecharge then self.onreceivechargefn(self.inst) end
+    end
+end
+
+return RechargeableAltar
